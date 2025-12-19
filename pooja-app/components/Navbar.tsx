12c1df5d403205a2536af37bedb6e4bdfb3c3d0a
Aman Bhardwaj <amanbhardwaj5931@gmail.com>
2025-12-19 19:29:42 +0530

    Make a dynamic button and changes the navbar css

--- pooja-app/components/Navbar.tsx
+++ pooja-app/components/Navbar.tsx
@@ -1,18 +1,22 @@
 import Link from "next/link";
+import Button from "./ui/Button";
 
 export default function Navbar() {
   return (
-    <nav className="w-full bg-white shadow sticky top-0 z-50">
+    <nav className="w-full sticky top-0 z-50">
       <div className="max-w-6xl mx-auto px-4 py-3 flex items-center justify-between">
         <Link href="/" className="text-xl font-bold text-orange-600">
           PujaGenie 🕉️
         </Link>
 
         <div className="flex gap-6 text-gray-700">
-          <Link href="/" className="hover:text-orange-600 transition">Home</Link>
-          <Link href="/poojas" className="hover:text-orange-600 transition">Poojas</Link>
-          <Link href="/booking" className="hover:text-orange-600 transition">Book Now</Link>
-          <Link href="/admin" className="hover:text-orange-600 transition">Admin</Link>
+          <Link href="/" className="hover:text-orange-600 text-white transition">Home</Link>
+          <Link href="/poojas" className="hover:text-orange-600 text-white transition">Poojas</Link>
+          <Link href="/booking" className="hover:text-orange-600 text-white transition">Book Now</Link>
+          <Link href="/admin" className="hover:text-orange-600 text-white transition">Admin</Link>
+        </div>
+        <div>
+            <Button>Click On Me</Button>
         </div>
       </div>
     </nav>
